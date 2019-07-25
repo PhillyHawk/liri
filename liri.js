31b69fad3a43ffffbf09ab7c52e211c5f988cda2
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-25 16:45:55 -0400

    added switch

--- liri.js
+++ liri.js
@@ -5,13 +5,32 @@ var keys = require("./keys.js");
 var Spotify =require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
+var command = process.argv[2];
+var value = process.argv.slice(3).join("");
 
-
+// switch (command) {
+//   case "concert-this":
+//     concertThis();
+//     break;
+//   case "spotify-this-song":
+//     spotifyThisSong();
+//     break;
+//   case "movie-this":
+//     movieThis();
+//     break;
+//   case "do-what-it-says":
+//     doWhatItSays();
+//     break;
+//   default:
+//     console.log("This is not a valid command, try again");
+//     console.log("------------------------------------------");
+// }
 //bands in town
-function getBands(artist){
-  var queryUrl= "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
+function getBands(){
+  var queryUrl= "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp";
+console.log(queryUrl);
 axios.get(queryUrl).then(function(response){
+  console.log(response)
   var jsonData=response.data;
   if(!jsonData.length){
     console.log("no results found");
@@ -27,10 +46,12 @@ axios.get(queryUrl).then(function(response){
 
   
 };
-getBands("Madonna");
+getBands();
 
 //spotify//
-spotify.search({ type: 'track', query: '1979' }, function(err, data) {
+function spotify(){
+
+spotify.search({ type: 'track', query: value }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
@@ -52,6 +73,5 @@ var song = data.tracks.items[0];
     console.log("-------Album-----");
     console.log(song.album.name);
 
-	});
-
-
+  });
+}
