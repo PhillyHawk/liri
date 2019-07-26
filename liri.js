c99f63b615d0afcae27364f7938c667c48dd6087
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-25 20:43:39 -0400

    Band working / spotify working/ ombd working/ do what working

--- liri.js
+++ liri.js
@@ -4,29 +4,14 @@ var moment= require("moment");
 var keys = require("./keys.js");
 var Spotify =require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
 
-var command = process.argv[2];
-var value = process.argv.slice(3).join("");
 
-// switch (command) {
-//   case "concert-this":
-//     concertThis();
-//     break;
-//   case "spotify-this-song":
-//     spotifyThisSong();
-//     break;
-//   case "movie-this":
-//     movieThis();
-//     break;
-//   case "do-what-it-says":
-//     doWhatItSays();
-//     break;
-//   default:
-//     console.log("This is not a valid command, try again");
-//     console.log("------------------------------------------");
-// }
+
+
+
 //bands in town
-function getBands(){
+function getBands(value){
   var queryUrl= "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp";
 console.log(queryUrl);
 axios.get(queryUrl).then(function(response){
@@ -46,12 +31,11 @@ axios.get(queryUrl).then(function(response){
 
   
 };
-getBands();
+// getBands();
 
 //spotify//
-function spotify(){
-
-spotify.search({ type: 'track', query: value }, function(err, data) {
+function spotifyThisSong(songChoice){ 
+spotify.search({ type: 'track', query: songChoice }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
@@ -75,3 +59,66 @@ var song = data.tracks.items[0];
 
   });
 }
+
+// getSpotify("1979");
+//ombd //
+
+function movieThis(movie) {
+  
+    var queryUrl = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+    axios.get(queryUrl).then(function(response) {
+      console.log("Title: " + response.data.Title);
+      console.log("Year: " + response.data.Year);
+      console.log("Imdb Rating: " + response.data.imdbRating);
+      console.log("Rotten Tomatoes: " + response.data.Metascore);
+      console.log("Country: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Actors: " + response.data.Actors);
+      console.log("------------------------------------");
+    });
+  }
+// movieThis("Aladin");
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
+
+    var dataArray = data.split(",");
+console.log(data)
+    if (dataArray.length === 2) {
+      userSwitch(dataArray[0], dataArray[1]);
+    } else if (dataArray.length === 1) {
+      userSwitch(dataArray[0]);
+    }
+  });
+};
+
+var userSwitch = function(userCommand, userFunction){
+  switch (userCommand) {
+    case "concert-this":
+      getBands(userFunction);
+      break;
+    case "spotify-this-song":
+      spotifyThisSong(userFunction);
+      break;
+    case "movie-this":
+      movieThis(userFunction);
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    default:
+      console.log("This is not a valid command, try again");
+      console.log("------------------------------------------");
+  }
+}
+
+var runUserSwitch= function(argOne, argTwo){
+  userSwitch(argOne, argTwo);
+}
+runUserSwitch(process.argv[2], process.argv.slice(3).join(" "))
+
+
+
+ 
+
